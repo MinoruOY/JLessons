fed5376f8d1234094ebd3480d2936f34c9fc097f
MinoruOY <red.n.ice@mail.ru>
2021-09-01 22:40:08 +0300

    Правка

--- src/com/explane5/CarManager.java
+++ src/com/explane5/CarManager.java
@@ -2,12 +2,13 @@ package com.explane5;
 
 public class CarManager {
 
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
 		Car FirstCar = new Car("TOYOTA Prius", "Green", 2008, 15000, 1200);
-		Car SecondCar = new Car("Renault Logan","Blue", 2000, 9700, 1600);
+		Car SecondCar = new Car("Renault Logan", "Blue", 2000, 9700, 1600);
 
 //		System.out.println("Model" + "	" + "Color" + "	" + "Year" + "	" + "Price $" + "	" + "Weight kg");
+//		System.out.println(FirstCar.CarColor);
 		System.out.println(FirstCar);
 		System.out.println(SecondCar);
 	}
